f25f013d673f9416d2c195607a08236c2666a4c7
jon <jonlyeatman@gmail.com>
2021-06-20 19:52:17 -0400

    Updated routing and DB file

--- routing/api.js
+++ routing/api.js
@@ -4,7 +4,7 @@ const db = require("../models");
 module.exports = function(app) {
 // Get routing 
     app.get("/api/workouts", (req, res) => {
-        db.Fitness.find({}).then(dbWorkout => {
+        db.Workout.find({}).then(dbWorkout => {
             res.json(dbWorkout);
         }).catch(err => {
             res.status(400).json(err);
@@ -12,7 +12,7 @@ module.exports = function(app) {
     })
 
     app.get("/api/workouts/range", ({}, res) => {
-        db.Fitness.find({}).then((dbWorkout) => {
+        db.Workout.find({}).then((dbWorkout) => {
             res.json(dbWorkout);
         }).catch(err => {
             res.status(400).json(err);
@@ -20,7 +20,7 @@ module.exports = function(app) {
     });
     // post routing
     app.post("/api/workouts/", (req, res) => {
-        db.Fitness.create(req.body).then((dbWorkout) => {
+        db.Workout.create(req.body).then((dbWorkout) => {
             res.json(dbWorkout);
             // console.log()
         }).catch(err => {
